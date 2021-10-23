d539f9745aff97fe985d3e3644cb5e6a52c3891b
hyunguk <hyunguk119@gmail.com>
2021-10-23 17:45:33 +0900

    Add new content.

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
       <img src="./assets/logo.png" class="logo" />
     </div>
 
-    <Container :게시물="게시물"/>
+    <Container :게시물="게시물" :step="step"/>
     <button @click="more">더보기</button>
 
     <div class="footer">
@@ -19,6 +19,13 @@
         <label for="file" class="input-plus">+</label>
       </ul>
     </div>
+
+<!--    <div v-if="step == 0">내용0</div>-->
+<!--    <div v-if="step == 1">내용1</div>-->
+<!--    <div v-if="step == 2">내용2</div>-->
+<!--    <button @click="step = 0">버튼0</button>-->
+<!--    <button @click="step = 1">버튼1</button>-->
+<!--    <button @click="step = 2">버튼2</button>-->
   </div>
 
 </template>
@@ -33,7 +40,9 @@ export default {
   name: 'App',
   data(){
     return {
-      게시물 : postdata
+      게시물 : postdata,
+      더보기 : 0,
+      step : 0,
     }
   },
   components: {
@@ -41,9 +50,10 @@ export default {
   },
   methods : {
     more(){
-      axios.get('https://codingapple1.github.io/vue/more0.json').then( 결과 => {
-        console.log(결과);
-        this.게시물.push(결과.data);
+      axios.get(`https://codingapple1.github.io/vue/more${this.더보기}.json`)
+          .then( 결과 => {
+            this.게시물.push(결과.data);
+            this.더보기++;
       })
     }
   },
